d9517078a6d06728a0c98d7bdeae237636c34e16
PostSonne <abiramova@gmail.com>
2020-02-27 20:25:43 +0300

    fix components

--- components/App.tsx
+++ components/App.tsx
@@ -1,27 +1,49 @@
 import * as React from "react";
 import { Filters } from "./Filters";
 import { TracksList } from "./TracksList";
+import { FC } from "react";
 
-class App extends React.Component {
-  state = {
-    tracksList: [],
-    // stops: Array(4).fill(true)
-  };
-  tracks: [];
-  getInfo(): Promise<void> {
+interface AppProps {
+  trackList?: [];
+  tracks?: [];
+}
+
+export const handleSetFilters= (type: string, index: number, checked: boolean) => {
+  let newFilters: any[] | never[] = [];
+  /*let newTrackList = [];
+  const filteringPredicate = (item) => {
+    return !!((item.stops < 4) && (newFilters[item.stops]));
+  };*/
+  switch (type) {
+    case "all": {
+      newFilters = Array(4).fill(checked)
+    }
+      break;
+    case "only": {
+      newFilters = Array(4).fill(false);
+      newFilters[index] = true
+    }
+      break;
+    case "one": {
+      // newFilters = this.state.stops.slice();
+      newFilters[index] = !newFilters[index]
+    }
+  }
+  /*newTrackList= this.TICKETS.filter((item) => filteringPredicate(item));
+  this.setState({stops: newFilters, ticketsList: newTrackList})*/
+};
+
+const App: FC<AppProps> = () => {
+  /*const getInfo = async (): Promise<void> => {
     return fetch("data.json")
     .then((response: Response) => response.json())
     .then((data) => {
-      this.tracks = data.tracks;
-      this.setState({tracksList: this.tracks});
-    })
+      tracks = data.tracks;
+      trackList = tracks})
     .catch((e) => {
       console.error("ERROR")
     })
-
-  }
-
-  render(){
+  };*/
     return (
         <>
           <div className="mainPage">
@@ -30,7 +52,6 @@ class App extends React.Component {
           </div>
         </>
     )
-  }
-}
+};
 
 export {App};
